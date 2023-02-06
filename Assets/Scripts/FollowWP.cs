edc32d6ca4aabb9e798e8136df33c69d8de2b291
Àngel Llompart <angelllompart@paucasesnovescifp.cat>
2023-02-06 10:57:23 +0100

    added navmesh

--- Assets/Scripts/FollowWP.cs
+++ Assets/Scripts/FollowWP.cs
@@ -11,24 +11,28 @@ public class FollowWP : MonoBehaviour
     [SerializeField] private TextMeshProUGUI txtDistance;
     [SerializeField] private GameObject goal;
     private float distance;
-    public float benzina = 10;
+    public float benzina = 50;
     public List<GameObject> waypoints;
     int currentWP = 0;
     GameObject destination;
-
+    UnityEngine.AI.NavMeshAgent agent;
+    private int x = 0;
     public float speed = 5.0f;
     public float rotSpeed = 10.0f;
 
     // Start is called before the first frame update
     void Start()
     {
+        distance = Vector3.Magnitude(goal.transform.position - transform.position);
+        agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
         waypoints = GameObject.FindGameObjectsWithTag("diposit").ToList();
+        CheckDestination();
     }
 
     // Update is called once per frame
     void Update()
     {
-        distance = Mathf.Sqrt(Mathf.Pow(goal.transform.position.x - transform.position.x,2) + Mathf.Pow(goal.transform.position.y - transform.position.y,2) + Mathf.Pow(goal.transform.position.z - transform.position.z,2));
+        distance = Vector3.Magnitude(goal.transform.position - transform.position);
         txtDistance.text = "Distancia: " + Mathf.Ceil(distance);
         /*if (Vector3.Distance(this.transform.position, waypoints[currentWP].transform.position) < 1)
         {
@@ -43,6 +47,7 @@ public class FollowWP : MonoBehaviour
 
         if (benzina > 0)
         {
+            /*
             Quaternion lookatWP = new Quaternion();
             if (waypoints.Count >= 1 && distance/speed > benzina)
             {
@@ -68,25 +73,53 @@ public class FollowWP : MonoBehaviour
             this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookatWP, rotSpeed * Time.deltaTime);
 
             this.transform.Translate(0, 0, speed * Time.deltaTime);
+            */
+            benzina -= Time.deltaTime * agent.velocity.magnitude;
             
-            benzina -= Time.deltaTime;
-            txtBenzina.text = "Benzina: " + Mathf.Ceil(benzina);
         }
+        else
+        {
+            agent.isStopped = true;
+        }
+        txtBenzina.text = "Benzina: " + Mathf.Ceil(benzina);
        
     }
 
 
+    private void CheckDestination()
+    {
+        if (distance - 3 <= benzina)
+        {
+            GoToGoal();
+        }
+        else
+        {
+            GoToNextWP();
+        }
+    }
+    public void GoToGoal(){
+        agent.SetDestination(goal.transform.position);
+    }
+    public void GoToNextWP() {
+        agent.SetDestination(waypoints[x].transform.position);
+        // g.AStar(currentNode, wps[4]);
+        // currentWP = 0;
+    }
+
+   
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("diposit"))
         {
             benzina += collision.gameObject.GetComponent<DipositManager>().ReadBenzine();
-            if (benzina >= 10)
+            if (benzina >= 50)
             {
-                benzina = 10;
+                benzina = 50;
             }
             waypoints.Remove(collision.gameObject);
             Destroy(collision.gameObject);
+            x++;
+            CheckDestination();
         } else if (collision.gameObject.CompareTag("Goal"))
         {
             Time.timeScale = 0;
